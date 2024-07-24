ae60c46b9225f334e6d1784a7c2bc76f29503b72
aman <amanjha3232@gmail.com>
2024-07-24 05:59:54 -0700

    Initial Commit

---
+++ bin/main.dart
@@ -0,0 +1,156 @@
+void main (){
+  // This is Single Line comment
+
+  /*
+  This is multiple line comment 
+  */
+  // print("Hello Dart");
+
+  // # Variable Name and Rules
+
+  // ## Valid Variable Name
+  // var name = "Aman";
+  // var f_name = "Rahul";
+  // var address1= "Ranchi";
+  // var $money = 100;
+
+  // ## Invalid Variable Name
+
+  // var 1person ="Sumit";
+  // var for = "Male";
+
+  //  # Printing 
+  
+  // print(name);
+  // print(f_name);
+  // print(address1);
+  // print($money);
+
+
+  // Built-in-DataTypes
+
+  // int cart_item = 10;
+  // print(cart_item);
+
+  // double price = 10.50;
+  // print(price);
+
+  // String city = "Ranchi";
+  // print(city);
+
+  // bool is_completed = true;
+
+  // print(is_completed);
+
+  // bool is_login = false;
+  // print(is_login);
+
+  // var quantity = 100;
+  // print(quantity);
+
+  // print(quantity.runtimeType);
+
+  // var cost =  100.50;
+  // print(cost);
+
+  // var gender = "Male";
+  // print(gender);
+
+  // var is_full = true;
+  // print(is_full);
+
+  // # Operators
+  //  ## Arthimetic Operators
+
+  // var a = 10;
+  // var b = 20;
+  // var c= 50;
+  // var d= 3;
+
+  // print(a + b);
+  // print(c - a);
+  // print(a * b);
+  // print(a / d);
+
+  // print(a ~/ d);
+
+  // print (a % d);
+
+  // ## Prefix and Postfix Increment and Decrement Operators
+
+  // var i =3 ;
+  // print(i);
+  // print(++i);
+  // print(i);
+
+  // var h = 3;
+  // print(h);
+  // print(h++);
+  // print(h);
+
+  //  Same goes for --(decrement operator) as well
+
+  //  ## Equality and Relational Operators
+
+  // var a = 100;
+  // var b = 200;
+  // print (a == b);
+  // print (a != b);
+  // print (a > b);
+  // print (a < b);
+  // print( a >= b);
+  // print( a <= b);
+
+  // ## Logical Operators
+  /*
+    true && true = true
+    true && false = false
+    false && true = false
+    false && false = false
+
+    true || true = true
+    true || false = true
+    false || true = true
+    false || false = false
+
+  */
+
+  // var a = 100;
+  // var b = 100;
+  // var c = 200;
+  // var d = 200;
+
+  // print((a == b) && (c == d));
+  // print((a!=b) || (c==d));
+
+  // ## Type Test Oprators
+
+  // var name = "Sonam";
+  // var number = 10;
+  // print(name is String);
+  // print(number is! int);
+
+  // ## Assignment Operators
+
+
+  // var n = 10;
+  // n += 5; // n = n + 5
+  // n -=5;  // n = n - 5
+  // n *=5;  // n = n * 5
+  
+  // print(n);
+
+  
+
+
+
+
+
+  
+
+
+
+
+
+
+}
