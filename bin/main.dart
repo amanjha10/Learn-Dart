c42eed9d3862bdf1c3b177f02b3d2733cf6fb76c
aman <amanjha3232@gmail.com>
2024-07-24 08:09:28 -0700

    Add string processing functions

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,5 @@
 void main (){
+
   // This is Single Line comment
 
   /*
@@ -60,6 +61,7 @@ void main (){
   // print(is_full);
 
   // # Operators
+
   //  ## Arthimetic Operators
 
   // var a = 10;
@@ -140,13 +142,117 @@ void main (){
   
   // print(n);
 
-  
+
+  //  # Conditional Expression
+
+  // var is_login = true;
+
+  // condition ? expr1 : expr2
+
+  // var user = is_login ? 'Aman' : 'Guest';
+  // print(user);
+
+  // var name = 'Aman';
+  // var user = name ?? 'Guest';
+
+  // print(user);
+
+
+  //  # String
+
+  // String name1 = 'Aman';
+  // print(name1);
+  // print(name1.runtimeType);
+
+  // String name2 = "Rahul";
+  // print(name2);
+
+  // var name3 = 'Sumit';
+  // print(name3);
+  // print(name3.runtimeType);
+
+  // var name4 = "Sumit";
+  // print(name4);
+  // print(name4.runtimeType);
+
+  // var s1 = "It's a Language";
+  // var s2 = "It\'s a Language";
+  // print(s1);
+  // print(s2);
+
+
+//   var mstr = '''This is 
+// multiline
+// String''';
+
+// print(mstr);
+
+//  var mstr2 = """This is 
+// multiline
+// String """;
+
+//   print(mstr2);
+
+//  ## Expression inside String
+
+// var name = 'Aman';
+// print('${name}');
+// print('$name');
+
+// print('My name is $name');
+// print('My name is ${name.toUpperCase()}');
+// print('My name is $name and I am good Boy');
+
+// var city = "Kathmandu";
+// print('Hello My Name is $name I am from $city');
+
+// ## String Concatenation
+
+// print('Hello Dart');
+// print('HEllo' + 'Aman');
+// print('HEllo' + ' ' + 'Aman');
+
+// var s1 = 'Hello';
+// var s2 = 'Sumit';
+// print(s1 + ' ' + s2);
+// print(s1 + ' My Name is ' + s2 );
+
+// // ## Row String
+// var s = r'In a raw String, not even \n gets special treatment.';
+// print(s);
+
+// ## String Properties and Methods
+
+// var name ="Aman";
+
+// print(name.length);
+// print(name.isEmpty);
+// print(name.isNotEmpty);
+// print(name.toUpperCase());
+// print(name.toLowerCase());
+// print(name.contains('n'));
+// print(name.padLeft(10));
+// print(name.padRight(10));
+
+// var str1 = '    HELLO     ';
+
+// print(str1);
+// print(str1.trim());
+// print(str1.trimLeft());
+// print(str1.trimRight());
+
+// var str2 = 'Hello&Dart';
+// print(str2.split('&'));
 
 
 
 
 
-  
+
+
+
+
+
 
 
 
---
+++ bin/tempCodeRunnerFile.dart
@@ -0,0 +1,4 @@
+var s1 = "It's a Language";
+  // var s2 = "It\'s a Language";
+  // print(s1);
+  // print(s2);
